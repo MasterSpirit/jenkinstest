6377fb39394c0b8f09d83e1c02a9b73108a16c90
masterspirit <qq306916172@126.com>
2019-03-18 20:20:25 +0800

    1233

---
+++ src/main/java/com/syf/jenkins/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.syf.jenkins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by syf on 2019/3/18.
+ */
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @GetMapping("/say")
+    public String Hello(){
+
+        return "123" ;
+    }
+}
